0c9767e28bf16a218a7e0f9f514a5dc9ff7caac3
haroldpineda <harold.pineda@monokera.com>
2023-08-23 13:12:46 -0500

    add step definition to see computational complexity algorithm used

--- features/step_definitions/algorithm_steps.rb
+++ features/step_definitions/algorithm_steps.rb
@@ -8,3 +8,13 @@ end
 Then('The text is processed and the number of characters, words and the density of use are identified') do
   @task.process_text(@text_extract)
 end
+
+And('The computational complexity of the algorithm used is shown') do
+  puts 'The complexity of this solution is simple because
+        as a function of time it depends on the number of
+        words and characters of the file to load, since
+        the more text the file contains, the longer it
+        will take to load, text division, count
+        calculate the frequency like this such as memory
+        usage to perform operations.'
+end
